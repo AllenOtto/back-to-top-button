a88943a37c65b8da4cd3e9bf1430dad93ea7f8ef
AllenOtto <kephamoturi@gmail.com>
2021-09-07 19:19:17 +0300

    Cache the DOM

---
+++ app.js
@@ -0,0 +1,11 @@
+var btt = document.getElementById('back-to-top'),
+    body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    scrollPos, docHeight;
+
+// Calculate the document height
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight)
+
+
+
