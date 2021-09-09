e08f7c342158608dc26df899c5523a27ea5aabb5
AllenOtto <kephamoturi@gmail.com>
2021-09-09 15:44:26 +0300

    Added show and hide functionality to btt-button based on scrollposition

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var btt = document.getElementById('back-to-top'),
 
 // Calculate the document height
 docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight)
+if (docHeight != 'undefined') {
+    offset = docHeight / 4;
+}
 
+// Add scroll event Listener
+window.addEventListener('scroll', () => {
+    scrollPos = body.scrollTop || docElem.scrollTop
 
+    if (scrollPos > offset) {
+        btt.classList.add('visible')
+    } else {
+        btt.classList.remove('visible')
+    }
+})
 
+btt.addEventListener('click', () => {
+    
+})
